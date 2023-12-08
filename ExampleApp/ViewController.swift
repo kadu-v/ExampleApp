61aebfad483f1b158a70cff8c35f61615cf36dcc
kadu-v <kazuonsunday@yahoo.co.jp>
2023-12-09 00:20:30 +0900

    Initial Commit

---
+++ ExampleApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExampleApp
+//
+//  Created by 池守和槻 on 2023/12/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
